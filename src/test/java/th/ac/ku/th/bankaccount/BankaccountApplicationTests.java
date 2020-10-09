aabeb769e52adf924da3a1769658e167dc401d24
Tiranan <tiranan.e@ku.th>
2020-10-09 23:31:13 +0700

    create bankaccount api

---
+++ src/test/java/th/ac/ku/th/bankaccount/BankaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.th.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
